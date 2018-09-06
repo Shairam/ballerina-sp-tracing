3d1bc789fe26efff527ff3cfce6f49c40ea1c20f
shairam <shaikrish27@gmail.com>
2018-09-06 17:42:16 +0530

    Add latest testdb.sql

--- testdb.sql
+++ testdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 09, 2018 at 01:01 PM
+-- Generation Time: Sep 06, 2018 at 02:10 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `marks`
+--
+
+CREATE TABLE `marks` (
+  `student_Id` int(11) NOT NULL,
+  `maths` int(3) NOT NULL,
+  `english` int(3) NOT NULL,
+  `science` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `marks`
+--
+
+INSERT INTO `marks` (`student_Id`, `maths`, `english`, `science`) VALUES
+(22, 65, 80, 100),
+(32, 80, 45, 72);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student`
 --
 
@@ -41,11 +62,20 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `age`, `name`, `mobNo`, `address`) VALUES
-(20, 24, 'Shai', 99876578, 'Colombo');
+(22, 20, 'Peter', 77123456, 'Colombo'),
+(25, 13, 'Kate', 112342345, 'Europe'),
+(32, 22, 'Krish', 771511551, 'Colombo-13'),
+(33, 21, 'Shairam', 771451511, 'Colombo');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `marks`
+--
+ALTER TABLE `marks`
+  ADD PRIMARY KEY (`student_Id`);
 
 --
 -- Indexes for table `student`
@@ -61,7 +91,17 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `marks`
+--
+ALTER TABLE `marks`
+  ADD CONSTRAINT `marks_ibfk_1` FOREIGN KEY (`student_Id`) REFERENCES `student` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
